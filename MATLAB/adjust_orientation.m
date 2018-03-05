a0cb8a3bc44e89303cf6cc329c1c78b51f915309
Tal Mezheritsky <talmezh@gmail.com>
2018-03-05 16:33:20 -0500

    Added trajectory to 3d model
    It is now possible to give a path of the same length as the nb of slices and the model will follow this path.
    
    Be careful of the frequency for a sinusoide path. must be 2 smaller than spatial frequency of acquisition.

--- MATLAB/adjust_orientation.m
+++ MATLAB/adjust_orientation.m
@@ -1,12 +1,16 @@
-function [delta] = adjust_orientation(slices, path)
+function [theta] = adjust_orientation(path)
 nb_slices = size(path, 1);
 delta = zeros(nb_slices,3);
+theta = zeros(size(delta));
 for i=1:nb_slices-1
     delta(i,1) = path(i+1,1)-path(i,1);
     delta(i,2) = path(i+1,2)-path(i,2);
     delta(i,3) = path(i+1,3)-path(i,3);
+    theta(i,1) = atan(delta(i,2)/delta(i,3));
+    theta(i,2) = atan(delta(i,1)/delta(i,3));
+    theta(i,3) = 0;
 end
 delta(nb_slices,:) = delta(nb_slices-1,:);
-%adjusted_slices = slices+delta;
+theta(nb_slices,:) = theta(nb_slices-1,:);
 end
 
--- MATLAB/test.m
+++ MATLAB/test.m
@@ -1,27 +1,30 @@
+close all
 clear all
 clc
 
-z = 0:86;
-
-%Parametric equation of path
-u = zeros(length(z),1);
-v = z;
-path(:,1) = u';
-path(:,2) = v';
-path(:,3) = z';
 
 
-OCT_path= "C:\Users\talmezh\Desktop\Annee3\H18\Projet3\Code\OCT_data\";
-lum = dicomread(OCT_path +"Lumen2.dcm");
-info = dicominfo(OCT_path +"Lumen2.dcm");
-%load (OCT_path + "image5_1.mat");
-% figure; imshow(image5(:,:,2))
-% figure;imshow(lum1(:,:,1));
-% figure;imshow(lum1(:,:,2));
+
+OCT_dir= "C:\Users\talmezh\Desktop\Annee3\H18\Projet3\Code\OCT_data\";
+lum = dicomread(OCT_dir +"Lumen1.dcm");
 dim = size(lum);
 lumen1 = zeros(dim([1 2 4]));
 nb_pts = zeros(dim(4),1);
 
+%Parametric equation of path
+% z = 0:4:4*(dim(4)-1);
+% u = 50*sin(pi*(1/(2*length(z)))*z);
+% v = 50*sin(pi*(1/(2*length(z)))*z);
+z = 0:dim(4);
+u = 100*sin(pi*(1/(2*length(z)))*z);
+v = 10*sqrt(z);
+
+path(:,1) = u;
+path(:,2) = v';
+path(:,3) = z';
+theta = adjust_orientation(path);
+
+%Extract points from dicom
 for i=1:size(lum,4)
     
     lumen1(:,:,i) = layers2image(lum(:,:,:,i));
@@ -29,12 +32,14 @@ for i=1:size(lum,4)
     nb_pts(i) = length(x);
 
     pts(1:nb_pts(i),1:2,i) = [x y];
-    pause(0.5);
 end
+
+% Generate Matrix for ply
+
 xyz = zeros(sum(nb_pts),6);
 for i=1:size(pts,3)
-   center = [dim(1)/2, dim(1)/2, i];
-   transform = make_transform3d(deg2rad(i/2),0,deg2rad(0));
+   center = path(i,1:3);
+   transform = make_transform3d(center(1), center(2), center(3), theta(i,1), theta(i,2), theta(i,3));
    
     if i ~= 1
         ini = sum(nb_pts(1:i-1))+1;
@@ -45,19 +50,17 @@ for i=1:size(pts,3)
     end
     range = ini:fin;
     
-    temp_x = pts(1:nb_pts(i),1,i);
-    temp_y = pts(1:nb_pts(i),2,i);
+    xyz(range,1) = pts(1:nb_pts(i),1,i)-dim(1)/2;
+    xyz(range,2) = pts(1:nb_pts(i),2,i)-dim(2)/2;
+    xyz(range,3) = 0;
+    
     for j=range
-        xyz(j,1:3) = transformPointsForward(transform,[temp_x(j) temp_y(j) 0]);
+        xyz(j,1:3) = transformPointsForward(transform,xyz(j,1:3));
     end
     
-    xyz(range,1) = xyz(range,1)-center(1);
-    xyz(range,2) = xyz(range,2)-center(2);
-    xyz(range,3) = xyz(range,3)+center(3);
-    
-    xyz(range,4) = -xyz(range,1);
-    xyz(range,5) = -xyz(range,2);
-    xyz(range,6) = -xyz(range,3);
+    xyz(range,4) = center(1)-xyz(range,1);
+    xyz(range,5) = center(2)-xyz(range,2);
+    xyz(range,6) = center(3)-xyz(range,3);
 end
 
 
